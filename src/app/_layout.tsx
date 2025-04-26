d5ce619cfd0701e1d876ec4e46628e0edcafd725
Bui Cong Hieu <buihieu9259@gmail.com>
2025-04-26 17:07:45 +0700

    fix bug

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -7,7 +7,7 @@ export default function RootLayout () {
             options={{headerShown: false, title: "Shop"}}
             />
             <Stack.Screen name="categories" 
-            options={{headerShown: true, title: "Categories"}}
+            options={{headerShown: false, title: "Categories"}}
             />
             <Stack.Screen name="product" 
             options={{headerShown: true, title: "Product"}}
